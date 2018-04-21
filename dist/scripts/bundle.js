6ce31904c37eee4ffb710bec45a8f69728493f57
Milan Milas <milanmilas@gmail.com>
2018-04-21 18:03:47 +0100

    React Forms - final

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -37616,6 +37616,8 @@ module.exports = AuthorForm;
 "use strict";
 
 var React = require('react');
+var router = require('react-router');
+var Link = router.Link;
 
 var AuthorList = React.createClass({displayName: "AuthorList",
     propTypes: {
@@ -37626,7 +37628,7 @@ var AuthorList = React.createClass({displayName: "AuthorList",
         var createAuthorRow = function(author) {
             return (
                 React.createElement("tr", {key: author.id}, 
-                    React.createElement("td", null, React.createElement("a", {href: "/authors/" + author.id}, author.id)), 
+                    React.createElement("td", null, React.createElement(Link, {to: "manageAuthor", params: {id: author.id}}, author.id)), 
                     React.createElement("td", null, author.firstName, " ", author.lastName)
                 )
             );
@@ -37650,7 +37652,7 @@ var AuthorList = React.createClass({displayName: "AuthorList",
 
 module.exports = AuthorList;
 
-},{"react":197}],205:[function(require,module,exports){
+},{"react":197,"react-router":28}],205:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -37713,6 +37715,14 @@ var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
 			errors: {},
 			dirty: false
 		};
+	},
+
+	componentWillMount: function() {
+		var authorId = this.props.params.id; //from the path 'author:id'
+
+		if(authorId){
+			this.setState({author: AuthorApi.getAuthorById(authorId)});
+		}
 	},
 
 	setAuthorState: function(event) {
@@ -37900,7 +37910,8 @@ var routes = (
   React.createElement(Route, {name: "app", path: "/", handler: require('./components/app')}, 
     React.createElement(DefaultRoute, {handler: require('./components/homePage')}), 
     React.createElement(Route, {name: "authors", handler: require('./components/authors/authorPage')}), 
-    React.createElement(Route, {name: "addAuthor", handler: require('./components/authors/manageAuthorPage')}), 
+    React.createElement(Route, {name: "addAuthor", path: "author", handler: require('./components/authors/manageAuthorPage')}), 
+    React.createElement(Route, {name: "manageAuthor", path: "author/:id", handler: require('./components/authors/manageAuthorPage')}), 
     React.createElement(Route, {name: "about", handler: require('./components/about/aboutPage')}), 
     React.createElement(NotFoundRoute, {handler: require('./components/notFoundPage')}), 
     React.createElement(Redirect, {from: "about-us", to: "about"}), 
